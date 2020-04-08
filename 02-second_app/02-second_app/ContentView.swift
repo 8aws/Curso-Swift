e68cd7fc2f420f43bba0a20a823582228ccb7bbd
8aws <36234794+8aws@users.noreply.github.com>
2020-04-08 17:59:49 +0200

    gestion de image y overlay

--- 02-second_app/02-second_app/ContentView.swift
+++ 02-second_app/02-second_app/ContentView.swift
@@ -13,9 +13,37 @@ struct ContentView: View {
         
         Image("skyline")
             .resizable()
-        //    .edgesIgnoringSafeArea(.all)
+         //   .edgesIgnoringSafeArea(.all)
             
-        .scaledToFill()
+        //.scaledToFill()
+            
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 300)
+          //  .clipped()
+            .clipShape(Capsule())
+            .opacity(0.2)
+        .overlay(
+            Text("Hola Corazon de melon y sandia que no te quiere ni tu tia que es mas alta que Mafalda")
+               .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+                .font(.system(.headline, design: .rounded))
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.gray)
+                .cornerRadius(10)
+                .opacity(0.7)
+                .padding(),
+                alignment: .bottom
+                
+            
+        )
+        
+        
+        
+        
+        
+        
+            
         
         
         
