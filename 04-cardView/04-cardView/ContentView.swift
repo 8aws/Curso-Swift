b646f49f83a332cca1916ce684ed3815823d9864
8aws <36234794+8aws@users.noreply.github.com>
2020-04-09 13:42:27 +0200

    bufff arreglado

---
+++ 04-cardView/04-cardView/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  04-cardView
+//
+//  Created by Manuel Rodriguez Alonso on 09/04/2020.
+//  Copyright © 2020 Manuel Rodriguez Alonso. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        
+    Text("hola")
+        
+    
+
+        
+        
+    }}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
